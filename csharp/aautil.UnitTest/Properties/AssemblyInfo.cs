2ea79fc62acb35a11167fceec03c0f912807fc90
dafashi <379002060@qq.com>
2020-09-26 13:34:40 +0800

    aautil csharp

---
+++ csharp/aautil.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("aautil.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("aautil.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("826ec0c2-f93e-4dee-9127-03f504c6c2c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ csharp/aautil/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace aautil
+{
+    public class Class1
+    {
+    }
+}
