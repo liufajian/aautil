e26abf021fa43648290a56d5eccbd86f98aebbd1
dafashi <379002060@qq.com>
2020-10-24 10:21:30 +0800

    converts

---
+++ csharp/aautil/Converts/Conversion.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace aautil
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Conversion
+    {
+        /// <summary> 
+        /// Gets the specified property value and converts it, if necessary. 
+        /// </summary> 
+        /// <typeparam name="T">The type to convert to.</typeparam> 
+        /// <param name="o">The object to convert.</param> 
+        /// <param name="defaultValue">The default value.</param> 
+        /// <returns></returns>
+        public static T Convert<T>(this object o, T defaultValue = default)
+        {
+            if (o == null) return defaultValue;
+            o = o.Convert(typeof(T), defaultValue);
+            return o is T o1 ? o1 : defaultValue;
+        }
+
+        /// <summary> 
+        /// Gets the specified property value and converts it, if necessary. 
+        /// </summary> 
+        /// <param name="o">The object to convert.</param> 
+        /// <param name="type">The type to convert to.</param> 
+        /// <param name="defaultValue">The default value.</param> 
+        /// <returns></returns> 
+        public static object Convert(this object o, Type type, object defaultValue)
+        {
+            if (o == null || o is DBNull) return defaultValue;
+            if (type.IsInstanceOfType(o)) return o;
+
+            try
+            {
+                if (o is string s)
+                {
+                    if (string.IsNullOrWhiteSpace(s)) return defaultValue;
+                    var result = Parse(type, s);
+                    if (result != null) return result;
+                }
+
+                var tc = TypeDescriptor.GetConverter(o.GetType());
+                return tc.CanConvertTo(type) ? tc.ConvertTo(o, type) : Parse(type, o) ?? defaultValue;
+            }
+            catch
+            {
+                return Parse(type, o.ToString()) ?? defaultValue;
+            }
+        }
+
+        /// <summary> 
+        /// Parses the specified object into the specified type. 
+        /// </summary> 
+        /// <param name="type">The type to parse into.</param> 
+        /// <param name="o">The object to parse.</param> 
+        /// <returns>An object of the specified type, if successful; otherwise, null.</returns> 
+        public static object Parse(Type type, object o)
+        {
+            var text = o?.ToString();
+
+            return string.IsNullOrWhiteSpace(text) ? default(object) : type.Name switch
+            {
+                "UInt16" when ushort.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "UInt32" when uint.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "UInt64" when ulong.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Int16" when short.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Int32" when int.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Int64" when long.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Byte" when byte.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Decimal" when decimal.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var v2) => v2,
+                "DateTime" when DateTime.TryParse(text, CultureInfo.InvariantCulture, DateTimeStyles.AllowInnerWhite | DateTimeStyles.AllowLeadingWhite | DateTimeStyles.AllowTrailingWhite | DateTimeStyles.AllowWhiteSpaces, out var v2) => v2,
+                "DateTimeOffset" when DateTimeOffset.TryParse(text, CultureInfo.InvariantCulture, DateTimeStyles.AllowInnerWhite | DateTimeStyles.AllowLeadingWhite | DateTimeStyles.AllowTrailingWhite | DateTimeStyles.AllowWhiteSpaces, out var v2) => v2,
+                "TimeSpan" when TimeSpan.TryParse(text, CultureInfo.InvariantCulture, out var v2) => v2,
+                "Boolean" => BooleanValues.Contains(text),
+                _ => null
+            };
+        }
+
+        private static readonly HashSet<string> BooleanValues;
+
+        static Conversion()
+        {
+            BooleanValues = new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "true", "1", "on", "yes" };
+        }
+    }
+}
---
+++ csharp/aautil/Converts/PocoToDictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace aautil
+{
+    /// <summary>
+    /// Converting (plain old) C# objects to dictionaries is easy with Reflection
+    /// </summary>
+    public static class PocoToDictionary
+    {
+        private static readonly MethodInfo AddToDicitonaryMethod = typeof(IDictionary<string, object>).GetMethod("Add");
+        private static readonly ConcurrentDictionary<Type, Func<object, IDictionary<string, object>>> Converters = new ConcurrentDictionary<Type, Func<object, IDictionary<string, object>>>();
+        private static readonly ConstructorInfo DictionaryConstructor = typeof(Dictionary<string, object>).GetConstructors().FirstOrDefault(c => c.IsPublic && !c.GetParameters().Any());
+
+        public static IDictionary<string, object> ToDictionary(this object obj) => obj == null ? null : Converters.GetOrAdd(obj.GetType(), o =>
+        {
+            var outputType = typeof(IDictionary<string, object>);
+            var inputType = obj.GetType();
+            var inputExpression = Expression.Parameter(typeof(object), "input");
+            var typedInputExpression = Expression.Convert(inputExpression, inputType);
+            var outputVariable = Expression.Variable(outputType, "output");
+            var returnTarget = Expression.Label(outputType);
+            var body = new List<Expression>
+            {
+                Expression.Assign(outputVariable, Expression.New(DictionaryConstructor))
+            };
+
+            body.AddRange(
+                from prop in inputType.GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.FlattenHierarchy)
+                where prop.CanRead && (prop.PropertyType.IsPrimitive || prop.PropertyType == typeof(string))
+                let getExpression = Expression.Property(typedInputExpression, prop.GetMethod)
+                let convertExpression = Expression.Convert(getExpression, typeof(object))
+                select Expression.Call(outputVariable, AddToDicitonaryMethod, Expression.Constant(prop.Name), convertExpression));
+
+            body.Add(Expression.Return(returnTarget, outputVariable));
+            body.Add(Expression.Label(returnTarget, Expression.Constant(null, outputType)));
+
+            var lambdaExpression = Expression.Lambda<Func<object, IDictionary<string, object>>>(
+                Expression.Block(new[] { outputVariable }, body), inputExpression);
+
+            return lambdaExpression.Compile();
+
+        })(obj);
+    }
+}
