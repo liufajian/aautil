cf8ead566de0a4b98b7d2b199d60958b2ee98392
dafashi <379002060@qq.com>
2020-12-04 18:58:12 +0800

    AAUtil.EFCore

---
+++ csharp/AAUtil.EFCore/IUnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Data
+{
+    /// <summary>
+    /// Defines the interface(s) for unit of work.
+    /// </summary>
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>
+        /// Gets the specified repository for the <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class;
+
+        /// <summary>
+        /// Saves all changes made in this context to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if sayve changes ensure auto record the change history.</param>
+        /// <returns>The number of state entries written to the database.</returns>
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        /// <summary>
+        /// Asynchronously saves all changes made in this unit of work to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
+        Task<int> SaveChangesAsync(bool ensureAutoHistory = false);
+
+        /// <summary>
+        /// Reject all changes
+        /// </summary>
+        void RejectChanges();
+
+        /// <summary>
+        /// Executes the specified raw SQL command.
+        /// </summary>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The number of state entities written to database.</returns>
+        int ExecuteSqlRaw(string sql, params object[] parameters);
+    }
+}
---
+++ csharp/AAUtil.EFCore/UnitOfWork.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace System.Data
+{
+    /// <summary>
+    /// Represents the default implementation of the <see cref="IUnitOfWork"/> and <see cref="IUnitOfWork{TContext}"/> interface.
+    /// </summary>
+    /// <typeparam name="TContext">The type of the db context.</typeparam>
+    public class UnitOfWork<TContext> : IUnitOfWork where TContext : DbContext
+    {
+        private bool disposed = false;
+        private Dictionary<Type, object> repositories;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UnitOfWork{TContext}"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public UnitOfWork(TContext context)
+        {
+            DbContext = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        /// <summary>
+        /// Gets the db context.
+        /// </summary>
+        /// <returns>The instance of type <typeparamref name="TContext"/>.</returns>
+        public TContext DbContext { get; }
+
+        /// <summary>
+        /// Gets the specified repository for the <typeparamref name="TEntity"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <returns>An instance of type inherited from <see cref="IRepository{TEntity}"/> interface.</returns>
+        public IRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+        {
+            if (repositories == null)
+            {
+                repositories = new Dictionary<Type, object>();
+            }
+
+            var type = typeof(TEntity);
+            if (!repositories.ContainsKey(type))
+            {
+                repositories[type] = new Repository<TEntity>(DbContext);
+            }
+
+            return (IRepository<TEntity>)repositories[type];
+        }
+
+        /// <summary>
+        /// Executes the specified raw SQL command.
+        /// </summary>
+        /// <param name="sql">The raw SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The number of state entities written to database.</returns>
+        public int ExecuteSqlRaw(string sql, params object[] parameters) => DbContext.Database.ExecuteSqlRaw(sql, parameters);
+
+        /// <summary>
+        /// Saves all changes made in this context to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <returns>The number of state entries written to the database.</returns>
+        public int SaveChanges(bool ensureAutoHistory = false)
+        {
+            if (ensureAutoHistory)
+            {
+                //_context.EnsureAutoHistory();
+            }
+
+            return DbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// Asynchronously saves all changes made in this unit of work to the database.
+        /// </summary>
+        /// <param name="ensureAutoHistory"><c>True</c> if save changes ensure auto record the change history.</param>
+        /// <returns>A <see cref="Task{TResult}"/> that represents the asynchronous save operation. The task result contains the number of state entities written to database.</returns>
+        public async Task<int> SaveChangesAsync(bool ensureAutoHistory = false)
+        {
+            if (ensureAutoHistory)
+            {
+                //_context.EnsureAutoHistory();
+            }
+
+            return await DbContext.SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Reject all changes
+        /// </summary>
+        public void RejectChanges()
+        {
+            foreach (var entry in DbContext.ChangeTracker.Entries().Where(e => e.State != EntityState.Unchanged))
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.State = EntityState.Detached;
+                        break;
+                    case EntityState.Modified:
+                    case EntityState.Deleted:
+                        entry.Reload();
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <param name="disposing">The disposing.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // clear repositories
+                    if (repositories != null)
+                    {
+                        repositories.Clear();
+                    }
+
+                    // dispose the db context.
+                    DbContext.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+    }
+}
